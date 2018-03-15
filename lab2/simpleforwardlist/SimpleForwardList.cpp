cae4e4d028e2ef0c2cc152ab0d5566b0966ae7a3
ZuzannaMisztal <zuzanna.misztal.agh@gmail.com>
2018-03-15 13:42:09 +0100

    SimpleForwardList - step 1 and 2 done

--- lab2/simpleforwardlist/SimpleForwardList.cpp
+++ lab2/simpleforwardlist/SimpleForwardList.cpp
@@ -4,33 +4,29 @@
 
 #include "SimpleForwardList.h"
 
-struct Node{
-    int value;
-    Node *next;
-};
-
-struct ForwardList{
-    //Node *Head;
-    int value;
-    Node *next;
-    //Node *Tail;
-};
-
 ForwardList *CreateNode(int value) {
-    /*Node new_node;
-    Node *nodeptr= &new_node;
-    nodeptr->value=value;
-    nodeptr->next= nullptr;
-    ForwardList *new_list;
-    new_list->Head=nodeptr;
-    //new_list->Tail= nullptr;
-    */
-    ForwardList *head;
+    ForwardList *head=new ForwardList;
     head->value=value;
     head->next= nullptr;
      return head;
 }
 
-void DestroyList(ForwardList head){
-    delete &head;
+void DestroyList(ForwardList *head) {
+    //ForwardList new_head=head;
+    while (head!= nullptr){
+        ForwardList *tmp=head->next;
+        delete head;
+        head=tmp;
+    }
+}
+
+ForwardList *PushFront(ForwardList *list, int value) {
+    //ForwardList *old_head=list;
+    if (list!= nullptr){
+        ForwardList *new_head=new ForwardList;
+        new_head->value=value;
+        new_head->next=list;
+        return new_head;
+    }
+    return nullptr;
 }
--- lab2/simpleforwardlist/SimpleForwardList.h
+++ lab2/simpleforwardlist/SimpleForwardList.h
@@ -6,10 +6,14 @@
 #define JIMP_EXERCISES_SIMPLEFORWARDLIST_H
 
 
-struct ForwardList;
-struct ListElement;
+typedef struct List{
+    int value;
+    struct List *next;
+}ForwardList;
 
 ForwardList *CreateNode(int value);
+void DestroyList(ForwardList *head);
+ForwardList *PushFront(ForwardList *list, int value);
 
 
 #endif //JIMP_EXERCISES_SIMPLEFORWARDLIST_H
